2ca7070dc7064ea7390f77c1c407ec26bfc456a7
Pharino Chum <pharino.chum@gmail.com>
2024-03-07 12:00:48 +0700

    add read image add write image

--- ECAM_S8_MV01_OpenCV01.py
+++ ECAM_S8_MV01_OpenCV01.py
@@ -1,11 +1,27 @@
-#OpenCV could be install in terminal via: pip3 install python-opencv
-import numpy as np #This is numpy package
-import cv2 #This is mudule of OpenCV
-import matplotlib as plt
+from __future__ import print_function
+import argparse
+import cv2
 
-img = cv2.imread('asset/img_sample_01.jpeg')
-b, g, r = cv2.split(img)
-img_matplot = cv2.merge([r,g,b])
-cv2.imshow('An Image', img)
+#construct the arguments parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", 
+                required=True,
+                help = "Path to the image")
+args = vars(ap.parse_args())
+
+#get image from parsing arguments
+img_color = cv2.imread(args["image"], 1) #read file as image color
+img_grayscale = cv2.imread(args["image"], 0) #read file as gray scale color
+img_unchanged = cv2.imread(args["image"], -1) #read file as it is. Do not change properties.
+cv2.imshow('A Color Image', img_color)
+cv2.imshow('A Gray Scale Image', img_grayscale)
+cv2.imshow('A Unchanged Properties Image', img_unchanged)
+
+#wait for a key stroke
 cv2.waitKey(0)
+
+#kills all open windows
 cv2.destroyAllWindows()
+
+#write grayscale image to "asset" folder
+cv2.imwrite("asset/sample_01_grayscale.jpg",img_grayscale)
